cadedb546ea40bc7a6433b4165b0f6b43cc94297
Latikesh Marathe <Latikesh29@gmail.com>
2026-09-16 10:18:05 +0530

    k

--- C/05_switch_case/program3_menu.c
+++ C/05_switch_case/program3_menu.c
@@ -3,10 +3,14 @@ int main() {
     int choice;
     scanf("%d", &choice);
     switch(choice) {
-        case 1: printf("Pizza"); break;
-        case 2: printf("Burger"); break;
-        case 3: printf("Pasta"); break;
-        case 4: printf("Sandwich"); break;
+        case 1: printf("Pizza"); 
+        break;
+        case 2: printf("Burger"); 
+        break;
+        case 3: printf("Pasta"); 
+        break;
+        case 4: printf("Sandwich"); 
+        break;
         default: printf("Invalid choice");
     }
     return 0;
